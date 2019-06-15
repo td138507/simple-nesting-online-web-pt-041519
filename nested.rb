9f8983359006d753cf34fd2b9fad7f5abf24445a
Taylor Doyle <tcd56@cornell.edu>
2019-06-15 23:47:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-programer_hash[:alan_kay]
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
